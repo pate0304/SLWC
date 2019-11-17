eae06f98d79e8f68d5f48cf0caa37c606b0a17e3
Jay Patel <pate0304@algonquinlive.com>
2019-11-17 17:46:33 -0500

    Steps to follow-
    get func for read and set wallpaper 
    build the api img downloader and follow with the app logic

--- app/src/main/java/ml/jaypatel/slwc/MainActivity.java
+++ app/src/main/java/ml/jaypatel/slwc/MainActivity.java
@@ -1,12 +1,31 @@
 package ml.jaypatel.slwc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.AtomicFile;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Bitmap img;
+
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         // Create flow for downling the 30 images per day from pexels api using our key.
         // WIll have to get permission from Pexels paid to use thier premium image download feature ***
         // using the free api key from pexels to test purpose only
-        WallpaperManager.getInstance(this).setBitmap();
+
+
+        String uri =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/" + "photo-1499084732479-de2c02d45fcc.jpg";
+        Log.d("uri",uri);
+        WallpaperManager wpm = WallpaperManager.getInstance(getApplicationContext());
+        InputStream ins = null;
+        try {
+            ins = new URL("absolute/path/of/image").openStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            wpm.setStream(ins);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
+
+
